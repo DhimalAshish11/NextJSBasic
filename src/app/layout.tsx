8e50e99c11b65e3d3ca1aadc960251db1d8d2258
Ashish Dhimal <128879625+DhimalAshish11@users.noreply.github.com>
2024-07-16 21:56:34 +1000

    u

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "../component/NavBar";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
